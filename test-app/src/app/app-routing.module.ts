814600955b33053d0ebed38ee46c125970027234
saidRaiss <raissaid90@gmail.com>
2020-11-30 09:51:16 +0100

    config routes

--- test-app/src/app/app-routing.module.ts
+++ test-app/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './home-page/home-page.component';
+import { DataComponent } from './data/data.component';
+import { SettingsComponent } from './settings/settings.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'data',
+    component: DataComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
